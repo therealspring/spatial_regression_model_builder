6b862a41f591c948eb0cb1e05fc19fc5576db147
Rich Sharp <rich@therealspring.org>
2022-04-20 11:37:03 -0700

    track script to build interaction tables

---
+++ build_interaction_table.py
@@ -0,0 +1,78 @@
+
+def main():
+    """Entry point."""
+import re
+
+if __name__ == '__main__':
+    main()
+
+    day_temp = [
+        'LST_Day_1km_01-01',
+        'LST_Day_1km_02-01',
+        'LST_Day_1km_03-01',
+        'LST_Day_1km_04-01',
+        'LST_Day_1km_05-01',
+        'LST_Day_1km_06-01',
+        'LST_Day_1km_07-01',
+        'LST_Day_1km_08-01',
+        'LST_Day_1km_09-01',
+        'LST_Day_1km_10-01',
+        'LST_Day_1km_11-01',
+        'LST_Day_1km_12-01',]
+    night_temp = [
+        'LST_Night_1km_01-01',
+        'LST_Night_1km_02-01',
+        'LST_Night_1km_03-01',
+        'LST_Night_1km_04-01',
+        'LST_Night_1km_05-01',
+        'LST_Night_1km_06-01',
+        'LST_Night_1km_07-01',
+        'LST_Night_1km_08-01',
+        'LST_Night_1km_09-01',
+        'LST_Night_1km_10-01',
+        'LST_Night_1km_11-01',
+        'LST_Night_1km_12-01',
+        ]
+    ndvi = [
+        'ndvi_06-01',
+        'ndvi_06-16',
+        'ndvi_07-01',
+        'ndvi_07-16',
+        'ndvi_08-01',
+        'ndvi_08-16',
+        'ndvi_09-01',
+        'ndvi_09-16',
+        'ndvi_10-01',
+        'ndvi_10-16',
+        ]
+    precip = [
+        'precipitation_01-01',
+        'precipitation_02-01',
+        'precipitation_03-01',
+        'precipitation_04-01',
+        'precipitation_05-01',
+        'precipitation_06-01',
+        'precipitation_07-01',
+        'precipitation_08-01',
+        'precipitation_09-01',
+        'precipitation_10-01',
+        'precipitation_11-01',
+        'precipitation_12-01',
+    ]
+
+    print('term1,term2')
+    precip_str = ''
+    night_str = ''
+    day_str = ''
+    for ndvi_id in ndvi:
+        month_int, day_int = [int(v) for v in re.match('.*_(\d\d)-(\d\d)', ndvi_id).groups()]
+        for month_index in range(1, month_int + (1 if day_int > 15 else 0)):
+            precip_id = precip[month_index-1]
+            night_id = night_temp[month_index-1]
+            day_id = day_temp[month_index-1]
+            precip_str += f'{ndvi_id},{precip_id}\n'
+            night_str += f'{ndvi_id},{night_id}\n'
+            day_str += f'{ndvi_id},{day_id}\n'
+    print(precip_str, end='')
+    print(night_str, end='')
+    print(day_str)
